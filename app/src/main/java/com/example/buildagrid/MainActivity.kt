19e1492fb03578b6f406da1bd54f7eafa55d1e72
Glenn Halls <gjhalls@gmail.com>
2023-02-23 17:39:10 +1100

    Add: composable topic card

--- app/src/main/java/com/example/buildagrid/MainActivity.kt
+++ app/src/main/java/com/example/buildagrid/MainActivity.kt
@@ -3,26 +3,63 @@ package com.example.buildagrid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.buildagrid.ui.theme.BuildAGridTheme
+import androidx.compose.ui.unit.dp
+import com.example.buildagrid.model.Topic
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BuildAGridTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+        }
+    }
+}
+
+@Composable
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card(elevation = 4.dp,) {
+        Row {
+            Image(
+                painter = painterResource(topic.imageResourceId),
+                contentDescription = stringResource(topic.stringResourceId),
+                modifier = Modifier
+                    .width(68.dp)
+                    .height(68.dp),
+                contentScale = ContentScale.Crop
+            )
+            Column (modifier = Modifier.padding(
+                end = 16.dp,
+                top = 16.dp)) {
+                Text(
+                    text = LocalContext.current.getString(topic.stringResourceId),
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(start = 16.dp, bottom = 8.dp)
+                )
+                Row {
+                    Image(
+                        painter = painterResource(R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(start = 16.dp, end = 8.dp)
+                            .size(12.dp)
+                            .align(Alignment.CenterVertically)
+                    )
+                    Text(
+                        text = topic.numberResourceId.toString()
+                    )
                 }
             }
         }
@@ -30,14 +67,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BuildAGridTheme {
-        Greeting("Android")
-    }
+@Preview
+private fun BuildAGridCardPreview() {
+    TopicCard(Topic(R.string.architecture, 159, R.drawable.architecture))
 }
