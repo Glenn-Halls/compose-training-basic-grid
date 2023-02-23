a78d1b2150a5ba1b93bc1b91e6daca6cf2204908
Glenn Halls <gjhalls@gmail.com>
2023-02-24 10:41:19 +1100

    Add: padding

--- app/src/main/java/com/example/buildagrid/MainActivity.kt
+++ app/src/main/java/com/example/buildagrid/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -17,20 +20,44 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.buildagrid.data.DataSource
 import com.example.buildagrid.model.Topic
+import com.example.buildagrid.ui.theme.BuildAGridTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            TopicApp()
         }
     }
 }
 
 @Composable
+fun TopicApp() {
+    BuildAGridTheme {
+        TopicGrid(topicList = DataSource().loadTopic())
+    }
+}
+
+@Composable
+fun TopicGrid(topicList: List<Topic>){
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.padding(8.dp)
+        ){
+            items(topicList) {topic ->
+                TopicCard(topic)
+            }
+    }
+}
+
+@Composable
 fun TopicCard(topic: Topic) {
-    Card(elevation = 4.dp) {
+    Card(elevation = 4.dp, modifier = Modifier.fillMaxWidth()) {
         Row {
             Image(
                 painter = painterResource(topic.imageResourceId),
@@ -72,5 +99,7 @@ fun TopicCard(topic: Topic) {
 @Composable
 @Preview
 private fun BuildAGridCardPreview() {
-    TopicCard(Topic(R.string.architecture, 159, R.drawable.architecture))
+    BuildAGridTheme {
+        TopicGrid(topicList = DataSource().loadTopic())
+    }
 }
